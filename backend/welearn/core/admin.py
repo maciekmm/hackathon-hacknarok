d919c71a7ca2a364e3560a61b4cce5f53f9cd8fb
Antoni Mleczko <antos.mleczko@gmail.com>
2018-03-10 15:27:26 +0100

    Db setup

--- backend/welearn/core/admin.py
+++ backend/welearn/core/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 
-# Register your models here.
+from core.models import Room, RoomCategory, Profile
+
+admin.site.register(Room)
+admin.site.register(RoomCategory)
+# admin.site.register(Profile)
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+# Define a new User admin
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInLine,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
