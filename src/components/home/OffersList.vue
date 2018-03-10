0cd5f0602a52e03dfa7e41c614825cadd241f8b8
Maciej Mionskowski <business@maciekmm.net>
2018-03-10 17:29:19 +0100

    Added transition to swipe panel

--- src/components/home/OffersList.vue
+++ src/components/home/OffersList.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="swipe-container" class="swipe mx-auto" fill-width grid-list-md text-xs-center>
-        <v-btn @click="toggle" v-if="this.collapsed">Show</v-btn>
-        <v-btn @click="toggle" v-if="!this.collapsed">Hide</v-btn>
+        <v-btn class="trigger-button" block round depressed color="primary" @click="toggle">
+          <span v-if="collapsed">Show</span>
+          <span v-else>Hide</span>
+        </v-btn>
         <v-layout row wrap>
             <v-flex xs12>
                 Test
@@ -40,9 +42,11 @@ export default {
       this.collapsed = !this.collapsed;
       let element = document.getElementById("swipe-container");
       if (this.collapsed) {
-        element.style.top = window.innerHeight - 60 + "px";
+        element.classList.remove('swipe-active')
+        //element.style.top = window.innerHeight - 60 + "px";
       } else {
-        element.style.top = "initial";
+        element.classList.add('swipe-active')
+        //element.style.top = "initial";
       }
     }
   }
@@ -50,13 +54,20 @@ export default {
 </script>
 <style>
 .swipe {
-  bottom: 0;
   right: 0;
   left: 0;
-  transition: top 2s;
+  transition: all 0.5s;
   text-align: center;
   width: 100%;
   position: fixed;
+  transform: translateY(-55px);
   background: #f7f7f7;
+}
+.swipe-active {
+  transform: translateY(-100%);
+}
+.trigger-button {
+  max-width: 70%;
+  margin: 8pt auto;
 }
 </style>
